c524da124eb0ac2447baf81225b61e25c0347d9d
princenayakpara <prince.nayakpara.cg@gmail.com>
2026-06-17 21:19:50 +0530

    feat: setup redux state management and mock data

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import uiReducer from './uiSlice';
+import userReducer from './userSlice';
+import dataReducer from './dataSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     ui: uiReducer,
+    user: userReducer,
+    data: dataReducer,
   },
 });
 
